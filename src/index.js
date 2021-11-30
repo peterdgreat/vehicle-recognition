23210e31754ce4aa85497d6375c72786b74a9c6f
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-30 11:39:38 +0100

    Removed Unnecessary Codes and Installed all Needed packages
    - Installed Redux for state Management
    - Installed axios for http requests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import store from './redux/configurestore';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,10 +12,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
