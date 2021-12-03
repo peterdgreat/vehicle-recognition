51c41cee4e4c5a6b6b09f61392df0ab66b003746
Ajayi Peter <peterdgreat2016@gmail.com>
2021-12-03 16:25:45 +0100

    Details Page Styled

--- src/component/Result.js
+++ src/component/Result.js
@@ -6,20 +6,47 @@ import '../styles/Result.css';
 export default function Result() {
       const dataResult =useSelector((state) => state.vehicleReducer.vehicles);
     return (
-       <div>
-             <NavLink to='/'>
-        <button>Back</button>
-</NavLink>
+    <div>
+          { dataResult !==undefined && (
+              <h1 className="text-center text-white"> <span>     {dataResult[0].color.name}</span>
+                <span>  { dataResult[0].car.make}</span>
+              </h1>
+          )
+              }
+                  <NavLink to='/'>
+        <button className="btn">Back</button>
+              </NavLink>
+         <div className="result card border">
+     
 
             {
               dataResult !==undefined && (
-                                     <div>
-                                      { dataResult[0].car.make} {dataResult[0].car.model} {dataResult[0].car.years} {dataResult[0].color.name}
-                                    </div>    
+                                     <ul className="list-unstyled w-100 h-100 list-group d-flex flex-column">
+                                    <li className="list-group-item h-25 border d-flex justify-content-between align-items-center">
+                                       <span>Car make</span>
+                                       <span> { dataResult[0].car.make}</span>
+                                      </li> 
+                                      <li className="list-group-item h-25 border d-flex justify-content-between align-items-center">
+                                      <span>Model</span>
+                                      <span>  {dataResult[0].car.model}</span>
+                                        </li > 
+                                        <li className="list-group-item h-25 border d-flex justify-content-between align-items-center">
+                                      <span>Year Range</span>
+                                      <span>    {dataResult[0].car.years} </span>
+                                        </li>
+                                  <li className="list-group-item h-25 border d-flex justify-content-between align-items-center">
+                                     <span>Car Color</span>
+                                     <span>     {dataResult[0].color.name}</span>
+                                  </li>
+                                    </ul>    
                                     )
+            }
+            {
+              dataResult ===undefined && <h1 className="h-100 d-flex justify-content-center align-items-center">No Car Uploaded</h1>
             }
             
        </div>
+    </div>
     )
 }
 
